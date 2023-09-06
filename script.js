418b4bda0e7153e39e07cf137fa7ff6134898054
Philip M-L <philip.marshalllockyer@outlook.com>
2023-09-06 15:45:52 +0100

    Begun adding modal for timezone changes

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
-const infoCard =  document.querySelector('.info')
-const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+const infoCard = document.querySelector(".info");
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 const updateDisplay = () => {
+  const currentTime = dayjs().tz(userTimeZone).format("HH:mm:ss");
+  const currentDate = dayjs().tz(userTimeZone).format("dddd D MMMM YYYY");
 
-    const currentTime = dayjs().tz(userTimeZone).format('HH:mm:ss')
-    const currentDate = dayjs().tz(userTimeZone).format('dddd D MMMM YYYY')
-
- const displayHTML = `
+  const displayHTML = `
 <div class="timezone">${userTimeZone}</div>
 <div class="time">${currentTime}</div>
 <div class="date">${currentDate}</div>`;
 
-infoCard.innerHTML = displayHTML
+  infoCard.innerHTML = displayHTML;
+};
 
-}
+updateDisplay();
 
-updateDisplay()
-
-setInterval(updateDisplay, 1000)
+setInterval(updateDisplay, 1000);
